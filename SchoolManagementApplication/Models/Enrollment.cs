08af625d108db4c9cdee27871ee7d82168a4cd71
pallavibhandary <53182343+pallavibhandary@users.noreply.github.com>
2021-08-25 20:05:17 +0530

    Lecturers module added

--- SchoolManagementApplication/Models/Enrollment.cs
+++ SchoolManagementApplication/Models/Enrollment.cs
@@ -18,8 +18,10 @@ namespace SchoolManagementApplication.Models
         public Nullable<decimal> Grade { get; set; }
         public int CourseId { get; set; }
         public int StudentId { get; set; }
+        public Nullable<int> LecturerId { get; set; }
     
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
+        public virtual Lecturer Lecturer { get; set; }
     }
 }
