396e73e73be4282510428bd12e0b8de6ec593421
pallavibhandary <53182343+pallavibhandary@users.noreply.github.com>
2021-08-25 15:29:58 +0530

    Add project files.

---
+++ SchoolManagementApplication/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace SchoolManagementApplication.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SchoolManagementApplication.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SchoolManagementApplication.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(SchoolManagementApplication.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ SchoolManagementApplication/Models/MetadataClasses/CoursesMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManagementApplication.Models
+{
+    public class CoursesMetadata
+    {
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Range(1,8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetadata))]
+    public partial class Course
+    {
+
+    }
+}
